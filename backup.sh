209cc01a97c7eaa7458a5432401ff805a938781d
Jeremiah ONeal <j.oneal2975@student.nu.edu>
2022-12-21 14:09:33 -0800

    Update

--- backup.sh
+++ backup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo chown -R www-data.www-data /var/www
+git config --global --add safe.directory /var/www/html
+git config --global user.email "j.oneal2975@student.nu.edu"
+git config --global user.name "Jeremiah ONeal"
+
+cd /var/www/html
 git add --all
 git commit -a -m "Update"  >> /var/www/html/backup.log 
 #git commit -m "[Updates 2]" >> /var/www/html/backup.log
